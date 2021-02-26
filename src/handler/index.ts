f773141530cfbecfa0520694bc41d8f53d937c4d
Aider <ideeer4@gmail.com>
2021-02-26 22:54:19 +0300

    Basic handler, scope

---
+++ src/handler/index.ts
@@ -0,0 +1,8 @@
+import parser from '../compiler';
+import scope from './sqope';
+
+export function getParser() {
+    parser.parser.yy.scope = new scope();
+    parser.parser.yy.ast = new parser.parser.yy.scope.ast_root();
+    return parser;
+}
