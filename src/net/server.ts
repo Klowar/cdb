d0732326a914c3f5125d2942ba3f73b616ab75b1
Aider <ideeer4@gmail.com>
2021-03-01 19:04:24 +0300

    Server is EventEmitter

--- src/net/server.ts
+++ src/net/server.ts
@@ -1,40 +1,32 @@
+import { EventEmitter } from 'events';
 import { Server, Socket } from 'net';
 import { logger } from '../core/logger';
-import { getParser } from '../handler';
 import { connection } from './connection';
 
-function server() {
+serverPrototype.prototype = new EventEmitter();
+function serverPrototype() {
+    this.addConnection = function(connection) {
+        this.connections.push(connection);
+    }
+    this.getPhysicalServer = function() {
+        return this.server;
+    }
+    this.onConnection = function(socket: Socket) {
+        const conn = new connection();
+        conn.setLogger(this.logger);
+        conn.init(socket);
+        return conn;
+    }
+}
+
+server.prototype = new serverPrototype();
+export function server() {
     this.connections = [];
     this.server = new Server();
+    this.logger = new logger();
     this.server.addListener("connection", (socket: Socket) => {
         const conn = this.onConnection(socket);
-        conn.addListener("data", (data: Buffer) => {
-            const str = data.toString();
-            var parser = getParser();
-            parser.parse(str);
-            socket.write(
-                JSON.stringify(parser.parser.yy.ast)
-            );
-        })
         this.addConnection(conn);
+        this.emit("connect", conn);
     });
-}
-
-server.prototype.getPhysicalServer = function() {
-    return this.server;
-}
-
-server.prototype.addConnection = function(connection) {
-    this.connections.push(connection);
-}
-
-server.prototype.onConnection = function(socket: Socket) {
-    const conn = new connection();
-    conn.setLogger(new logger());
-    conn.init(socket);
-    return conn;
-}
-
-export function getServer() {
-    return new server();
 }
