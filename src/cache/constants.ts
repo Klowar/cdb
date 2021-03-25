ac495ffb244de5269813b7f3508479319231240d
Aider <ideeer4@gmail.com>
2021-03-25 22:14:39 +0300

    Cache layer

---
+++ src/cache/constants.ts
@@ -0,0 +1,3 @@
+
+// Max 1000 records
+export const MAX_ENTITY_CACHE = 1000;
---
+++ src/cache/index.ts
@@ -0,0 +1,18 @@
+
+
+function Cache(config: {}) {
+    // Each union has entity map and each entity map has padding to record mapping
+    this.uniounCache = new Map<string, Map<string, Map<string, string>>>();
+}
+
+Cache.prototype.write = function (offset, data) {
+    console.log(this, "Tries to write");
+}
+
+Cache.prototype.read = function (offset, amount) {
+    console.log(this, "Tries to read");
+}
+
+export function getCache(config: {}) {
+    return new Cache(config);
+}
