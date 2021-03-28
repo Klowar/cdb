e20b20d1a106a1d87459dde07f9a796ffc645eb6
Aider <ideeer4@gmail.com>
2021-03-28 22:56:58 +0300

    Query builder layer

---
+++ src/builder/index.ts
@@ -0,0 +1,17 @@
+import { ExecutorType } from './../exec/index';
+
+export type QueryBuilderType = {
+    executor: ExecutorType
+}
+
+function QueryBuilder(config) {
+    this.executor = config.executor;
+}
+
+QueryBuilder.prototype.process = function (request) {
+    console.log("Checks for other request to same data to compose", request);
+}
+
+export function getQueryBuilder(config) {
+    return new QueryBuilder(config);
+}
