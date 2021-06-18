d7af291396e16d9df53e443b5ee6d9e2173aa137
Aider <ideeer4@gmail.com>
2021-06-19 01:15:54 +0300

    Timestamp prop

--- src/globals.ts
+++ src/globals.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import winston from 'winston';
+import winston, { format } from 'winston';
 
 const DATA_FOLDER = "db";
 const ENTITY_CONFIG_PATH = "meta.json";
@@ -9,9 +9,13 @@ export const CONFIG_ROOT = join(__dirname, DATA_FOLDER, ENTITY_CONFIG_PATH);
 
 export const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: format.combine(
+        winston.format.json(),
+        format.timestamp()
+    ),
     // defaultMeta: { service: 'user' },
     transports: [
+        new winston.transports.File({ filename: 'log.log', level: 'info' }),
         new winston.transports.Console({
             format: winston.format.simple(),
         })
