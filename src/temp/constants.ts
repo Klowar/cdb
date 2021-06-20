300e82e57209e8c7a390cad600c846129af20154
Aider <ideeer4@gmail.com>
2021-06-20 11:59:21 +0300

    Run config, constants update

--- src/temp/constants.ts
+++ src/temp/constants.ts
@@ -1,11 +1,11 @@
 
 // 128 Mb
 export const DEFAULT_LOCK_SIZE = 1024 * 1024 * 128;
-// 8 seconds
-export const DEFAULT_FLUSH_TICK = 8 * 1000;
+// 2 seconds
+export const DEFAULT_FLUSH_TICK = 2 * 1000;
 // Forced flush after
 export const DEFAULT_FORCED_FLUSH_AFTER = 4 * DEFAULT_FLUSH_TICK;
 // Max one flush size
-export const MAX_FLUSH_SIZE = 512;
+export const MAX_FLUSH_SIZE = 256;
 // Flush call waterlinee
 export const FLUSH_CALL_WATERLINE = 128;
